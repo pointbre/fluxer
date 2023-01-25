38c66ed1e22467e825e1528c1ea82c500095c078
Lucas Kim <pointbre@gmail.com>
2023-01-26 01:02:57 +1300

    Saved

--- fluxer-core/src/test/java/com/github/pointbre/core/TcpServerLinkTest.java
+++ fluxer-core/src/test/java/com/github/pointbre/core/TcpServerLinkTest.java
@@ -1,7 +1,5 @@
 package com.github.pointbre.core;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
